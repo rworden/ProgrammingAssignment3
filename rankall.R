9e7bb56025bd526c833979122ab288858bbc6083
rworden <rossworden@gmail.com>
2014-06-17 11:28:20 -0500

    mid point, figure out maxRanks

--- rankall.R
+++ rankall.R
@@ -7,7 +7,6 @@ rankall <- function(outcome, num = "best") {
     uniqueOutcomes<-as.character(c("heart attack","heart failure","pneumonia"))
     
     # Kill the function if given invalid inputs
-    if(!(state %in% uniqueStates)) {stop("invalid state")}
     if(!(outcome %in% uniqueOutcomes)) {stop("invalid outcome")}
     
     # map outcome to proper columns
@@ -26,6 +25,25 @@ rankall <- function(outcome, num = "best") {
     stringRemove = "Not Available"
     #remove any row that does not appear to have data
     rankAllTable<-subset(rankAllTable, rankAllTable[,colIndex] != stringRemove)
+    
+    # count hospitals to determine "worst" number
+    ## this to be by state
+    maxRow<-nrow(rankTable) 
+    
+    ##### need to figure out how to get count of rows for each state
+    ##### then i can set maxRow to translate "worst"
+      maxRows<-tapply(rankTable[,7],rankTable[,2], sum)
+    # maxRows<-by(rankTable[,7],rankTable[,7], nrow)
+      print(maxRows)
+    # Translate strings and change to numeric on rank
+    numTranslate = numeric()
+    numTranslate<-if(num == "best") {
+        1
+    }
+    else if (num == "worst") {
+        as.numeric(maxRow)
+    }
+    else as.numeric(num)
 
 ## For each state, find the hospital of the given rank
 ## Return a data frame with the hospital names and the ## (abbreviated) state name
